d3fdb5426a0e09337bf8e8b301f5bb85f03b0328
Sawolf <Savipon@yandex.ru>
2024-10-01 10:24:25 +0300

    main Screen and started developing

---
+++ WordsReminder/WordsReminderTests/WordsReminderTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordsReminderTests.swift
+//  WordsReminderTests
+//
+//  Created by Савва Пономарев on 01.10.2024.
+//
+
+import Testing
+@testable import WordsReminder
+
+struct WordsReminderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
